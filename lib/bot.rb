bbfdeafcd65c25fd6ee7a5e4ccb83e7ce631ecb3
Vyacheslav Alexeev <alexeev.corp@gmail.com>
2020-08-16 13:52:00 +0300

    pid & log files management

--- lib/bot.rb
+++ lib/bot.rb
@@ -1,3 +1,4 @@
+require 'fileutils'
 require_relative 'valera'
 require 'telegram/bot'
 
@@ -8,16 +9,108 @@ class Bot
 
   def initialize(options)
     @options = options
+
+    options[:logfile] = File.expand_path(logfile) if logfile?
+    options[:pidfile] = File.expand_path(pidfile) if pidfile?
+
     redis_adapter = Valera::Adapters::Redis.new(url: options[:redis_url])
     @chain = Valera::Chain.new(redis_adapter)
     @builder = Valera::ChainBuilder.new(chain)
   end
 
   def run!
+    check_pid
+    daemonize if daemonize?
+    write_pid
+
+    if logfile?
+      redirect_output
+    elsif daemonize?
+      suppress_output
+    end
+
     run_telegram_bot
   end
 
   private
+
+  def daemonize?
+    options[:daemonize]
+  end
+
+  def logfile
+    options[:logfile]
+  end
+
+  def pidfile
+    options[:pidfile]
+  end
+
+  def logfile?
+    !logfile.nil?
+  end
+
+  def pidfile?
+    !pidfile.nil?
+  end
+
+  def write_pid
+    if pidfile?
+      begin
+        FileUtils.mkdir_p(File.dirname(pidfile), :mode => 0755)
+        File.open(pidfile, ::File::CREAT | ::File::EXCL | ::File::WRONLY){|f| f.write("#{Process.pid}") }
+        at_exit { File.delete(pidfile) if File.exists?(pidfile) }
+      rescue Errno::EEXIST
+        check_pid
+        retry
+      end
+    end
+  end
+
+  def check_pid
+    if pidfile?
+      case pid_status(pidfile)
+      when :running, :not_owned
+        puts "A server is already running. Check #{pidfile}"
+        exit(1)
+      when :dead
+        File.delete(pidfile)
+      end
+    end
+  end
+
+  def pid_status(pidfile)
+    return :exited unless File.exists?(pidfile)
+    pid = ::File.read(pidfile).to_i
+    return :dead if pid == 0
+    Process.kill(0, pid) # check process status
+    :running
+  rescue Errno::ESRCH
+    :dead
+  rescue Errno::EPERM
+    :not_owned
+  end
+
+  def daemonize
+    exit if fork
+    Process.setsid
+    exit if fork
+    Dir.chdir "/"
+  end
+
+  def redirect_output
+    FileUtils.mkdir_p(File.dirname(logfile), :mode => 0755)
+    FileUtils.touch logfile
+    File.chmod(0644, logfile)
+    $stderr.reopen(logfile, 'a')
+    $stdout.reopen($stderr)
+    $stdout.sync = $stderr.sync = true
+  end
+
+  def suppress_output
+    $stderr.reopen('/dev/null', 'a')
+    $stdout.reopen($stderr)
+  end
 
   def run_telegram_bot
     Telegram::Bot::Client.run(options[:telegram_token]) do |bot|
