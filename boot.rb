771cf99752277e2b8ced2ffbe6c4e9413fe2c486
Vyacheslav Alexeev <alexeev.corp@gmail.com>
2021-12-02 22:38:07 +0800

    Get out boot; New script to dump data

---
+++ boot.rb
@@ -0,0 +1,12 @@
+require 'pathname'
+lib_path ||= File.expand_path("../lib", Pathname.new(__FILE__).realpath)
+$LOAD_PATH.unshift(lib_path) unless $LOAD_PATH.include?(lib_path)
+ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", Pathname.new(__FILE__).realpath)
+
+require 'rubygems'
+require 'bundler/setup'
+
+Bundler.require(:default)
+
+require 'logger'
+require 'bot'
