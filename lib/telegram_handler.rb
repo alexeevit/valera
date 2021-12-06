4ef8ba6c29f4103de5584b5da5495901c1e75793
Vyacheslav Alexeev <alexeev.corp@gmail.com>
2021-12-06 23:21:54 +0800

    Listen for mentions and replies

--- lib/telegram_handler.rb
+++ lib/telegram_handler.rb
@@ -2,7 +2,7 @@ require_relative 'valera'
 require 'telegram/bot'
 
 class TelegramHandler
-  attr_reader :options, :redis_adapter
+  attr_reader :options, :redis_adapter, :me, :chain, :builder
 
   def initialize(options = {})
     @options = options
@@ -11,25 +11,28 @@ class TelegramHandler
 
   def run!
     Telegram::Bot::Client.run(options[:token]) do |bot|
+      set_me(bot)
+
       bot.listen do |message|
         next unless message
-        chain = Valera::Chain.new(redis_adapter, "telegram:#{message.chat.id}")
-        builder = Valera::ChainBuilder.new(chain)
+
+        unless me['can_read_all_group_messages']
+          bot.api.send_message(chat_id: message.chat.id, text: 'У меня нет доступа к сообщениям')
+        end
+
+        set_chain_and_builder(message)
 
         begin
+          if message.reply_to_message&.from&.username == me['username'] || message.text.match?(me['username'])
+            bot.api.send_message(chat_id: message.chat.id, reply_to_message_id: message.message_id, text: generate)
+            next
+          end
+
           case message.text
           when /хуйня/i
             bot.api.send_message(chat_id: message.chat.id, reply_to_message_id: message.message_id, text: 'А может это ты хуйня?')
           when '/generate'
-            generator = Valera::Generator.new(chain)
-            sentence_size = rand(30) + 10
-            generated_text = generator.get(sentence_size)
-
-            if String(generated_text).empty?
-              bot.api.send_message(chat_id: message.chat.id, text: 'Мне нечего вам сказать')
-            else
-              bot.api.send_message(chat_id: message.chat.id, text: generated_text)
-            end
+            bot.api.send_message(chat_id: message.chat.id, text: generate)
           when '/stats'
             stats = builder.stats
             stats_text = "Количество пар: #{stats[:pairs_count]}\nКоличество переходов: #{stats[:transitions_count]}"
@@ -51,5 +54,24 @@ class TelegramHandler
         end
       end
     end
+  end
+
+  private
+
+  def generate
+    generator = Valera::Generator.new(chain)
+    sentence_size = rand(30) + 10
+    generated_text = generator.get(sentence_size)
+    return 'Мне нечего вам сказать' if String(generated_text).empty?
+    generated_text
+  end
+
+  def set_me(bot)
+    @me = bot.api.get_me['result']
+  end
+
+  def set_chain_and_builder(message)
+    @chain = Valera::Chain.new(redis_adapter, "telegram:#{message.chat.id}")
+    @builder = Valera::ChainBuilder.new(chain)
   end
 end
