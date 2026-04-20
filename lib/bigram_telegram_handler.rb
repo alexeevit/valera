46386dc048f7629df6ae48ee228fc121302a66e5
Slava <alexeev.corp@gmail.com>
2026-04-20 12:57:02 +0200

    Train on all messages

--- lib/bigram_telegram_handler.rb
+++ lib/bigram_telegram_handler.rb
@@ -16,13 +16,13 @@ class BigramTelegramHandler
         begin
           model = Valera::Bigram::Model.new(redis, "telegram:#{message.chat.id}")
 
+          Valera::Bigram::Trainer.new(model).call(message.text) if message.text
+
           if mentioned?(message)
             mention = "@#{message.from.username}"
             text = Valera::Bigram::Generator.new(model, mention: mention).call
             text = 'Мне нечего вам сказать' if text.empty?
             bot.api.send_message(chat_id: message.chat.id, reply_to_message_id: message.message_id, text: text)
-          elsif message.text
-            Valera::Bigram::Trainer.new(model).call(message.text)
           end
         rescue => e
           bot.api.send_message(chat_id: message.chat.id, text: "Something wrong happened: #{e.class}: #{e.message}")
